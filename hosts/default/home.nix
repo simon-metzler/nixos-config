8e89a7061325b11b992938a1d5664007c3b95fab
simon-metzler <simonmetzler@gmx.at>
2025-05-02 15:15:42 +0200

    added basic hyprland config

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -1,10 +1,18 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "simon";
-  home.homeDirectory = "/home/simon";
 
-  home.stateVersion = "24.11"; 
+  imports = [
+    ../../modules/home-manager/bundle.nix
+  ];
+
+
+  home = {
+    username = "simon";
+    homeDirectory = "/home/simon";
+    stateVersion = "24.11"; 
+  }
+
 
   programs.home-manager.enable = true;
 }
---
+++ modules/home-manager/hyprland.nix
@@ -0,0 +1,76 @@
+{
+  wayland.windowManager.hyprland ={
+    enabled = true;
+
+    settings = {
+      "$mainMod" = "SUPER";
+
+      monitor = [
+        "HDMI-A-1,1920x1080,0x0,1"
+        "DP-2,1920x1080,1920x0,1"
+
+      ];
+
+      input = {
+        kb_layout = "de";
+
+        follow_mouse = 1;
+
+        touchpad = {
+          natural_scroll = false;
+        };
+
+        sensitivity = 0;
+      };
+
+      general = {
+        gaps_in = 2;
+        gaps_out = 4;
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
+        layout = "dwindle";
+      };
+
+      decoration {
+        drop_shadow = true;
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(1a1a1aee)";
+      };
+
+      animations = {
+        enabled = true;
+
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+        animation = [
+          "windows,     1, 7,  myBezier"
+          "windowsOut,  1, 7,  default, popin 80%"
+          "border,      1, 10, default"
+          "borderangle, 1, 8,  default"
+          "fade,        1, 7,  default"
+          "workspaces,  1, 6,  default"
+        ];
+      };
+
+      dwindle = {
+        pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+        preserve_split = true; # you probably want this
+      };
+
+      master = {
+        new_is_master = true;
+      };
+
+      bind = [
+        "$mainMod, Q, exec, $terminal"
+        "$mainMod, C, killactive"
+        "$mainMod, M, exit"
+        "$mainMod, V, togglefloating"
+        "$mainMod, R, exec, rofi -show drun"
+
+      ];
+
+    };
+  };
+}
