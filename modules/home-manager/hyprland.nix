a249b4760f7f17e777b69b7ea97f04fc3bf07aaf
simon-metzler <simonmetzler@gmx.at>
2025-05-02 18:21:21 +0200

    minimal hyprland config

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -1,6 +1,7 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
+    xwayland.enable = true;
 
     settings = {
       "$mainMod" = "SUPER";
@@ -21,45 +22,6 @@
         };
 
         sensitivity = 0;
-      };
-
-      general = {
-        gaps_in = 2;
-        gaps_out = 4;
-        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-        "col.inactive_border" = "rgba(595959aa)";
-        layout = "dwindle";
-      };
-
-      decoration = {
-        drop_shadow = true;
-        shadow_range = 4;
-        shadow_render_power = 3;
-        "col.shadow" = "rgba(1a1a1aee)";
-      };
-
-      animations = {
-        enabled = true;
-
-        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-
-        animation = [
-          "windows,     1, 7,  myBezier"
-          "windowsOut,  1, 7,  default, popin 80%"
-          "border,      1, 10, default"
-          "borderangle, 1, 8,  default"
-          "fade,        1, 7,  default"
-          "workspaces,  1, 6,  default"
-        ];
-      };
-
-      dwindle = {
-        pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-        preserve_split = true; # you probably want this
-      };
-
-      master = {
-        new_is_master = true;
       };
 
       bind = [
