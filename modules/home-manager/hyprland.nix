f58705a43413299fc744d9c5e34ffabcd0ca7824
simon-metzler <simonmetzler@gmx.at>
2025-05-02 19:16:47 +0200

    worked on hyprland.nix

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -7,10 +7,43 @@
       "$mainMod" = "SUPER";
 
       monitor = [
-        "HDMI-A-1,1920x1080,0x0,1"
-        "DP-2,1920x1080,1920x0,1"
-
+        "DP-2,1920x1080,0x0,1"
+        "HDMI-A-1,1920x1080,1920x0,1"
       ];
+
+
+      general =  {
+        gaps_in = 2;
+        gaps_out = 4;
+
+        border_size = 0;
+
+        resize_on_border = false;
+
+        layout = "dwindle";
+      };
+
+      decoration = {
+        active_opacity = 1.0;
+        inactive_opacity = 0.9;
+
+        shadow {
+          enabled = true;
+          range = 4;
+          render_power = 3;
+          color = "rgba(1a1a1aee)";
+        };
+
+        blur {
+          enabled = true
+          size = 3
+          passes = 1
+
+          vibrancy = 0.1696
+        };
+
+
+      };
 
       input = {
         kb_layout = "de";
@@ -25,12 +58,21 @@
       };
 
       bind = [
-        "$mainMod, Q, exec, $terminal"
+        "$mainMod, Q, exec, alacritty"
         "$mainMod, C, killactive"
         "$mainMod, M, exit"
         "$mainMod, V, togglefloating"
         "$mainMod, R, exec, rofi -show drun"
 
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, down, movefocus, d"
+      ];
+
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
       ];
 
     };
--- modules/nixos/packages.nix
+++ modules/nixos/packages.nix
@@ -11,5 +11,6 @@
     rofi
     git
     gh
+    alacritty
   ];
 }
