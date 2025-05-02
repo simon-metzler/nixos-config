422a88a945fe0639afc933e06e1d332f73d3db0b
simon-metzler <simonmetzler@gmx.at>
2025-05-02 19:18:25 +0200

    fixed missing chars in hyprland.nix

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -27,19 +27,19 @@
         active_opacity = 1.0;
         inactive_opacity = 0.9;
 
-        shadow {
+        shadow = {
           enabled = true;
           range = 4;
           render_power = 3;
           color = "rgba(1a1a1aee)";
         };
 
-        blur {
-          enabled = true
-          size = 3
-          passes = 1
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
 
-          vibrancy = 0.1696
+          vibrancy = 0.1696;
         };
 
 
