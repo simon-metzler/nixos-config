b4e14e30f50f0f97ae73a7d9f6498a3daf0797f4
simon-metzler <simonmetzler@gmx.at>
2025-05-04 11:56:50 +0200

    fixed typo in hyprpaper.nix

--- modules/home-manager/hyprpaper.nix
+++ modules/home-manager/hyprpaper.nix
@@ -3,10 +3,10 @@
     enable = true;
 
     settings = {
-      preload = [ "../wallpapers.left_house.jpg" "../wallpapers.right_house.jpg" ];
+      preload = [ "../wallpapers/left_house.jpg" "../wallpapers/right_house.jpg" ];
       wallpaper = [
-        "DP-2,../wallpapers.left_house.jpg"
-        "HDMI-A-1,../wallpapers.right_house.jpg"
+        "DP-2,../wallpapers/left_house.jpg"
+        "HDMI-A-1,../wallpapers/right_house.jpg"
       ];
     }
   }
