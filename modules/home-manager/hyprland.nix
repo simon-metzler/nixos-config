0571dab0f222c71c8033a6367dc5b494c94c2593
simon-metzler <simonmetzler@gmx.at>
2025-05-06 18:34:14 +0200

    fixed typo in hyprland config

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -82,8 +82,8 @@
         builtins.concatLists (builtins.genList (i:
             let ws = i + 1;
             in [
-              "$mod, code:1${toString i}, workspace, ${toString ws}"
-              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+              "$mainMod, code:1${toString i}, workspace, ${toString ws}"
+              "$mainMod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
             ]
           )
           9)
